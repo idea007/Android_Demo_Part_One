4299de29908b377a12025657c3f1f86f403114f4
idea <1063292693@qq.com>
2016-03-21 19:28:08 +0800

    update
    添加butterknife

--- AndroidDemoPartOne/app/src/main/java/com/example/idea/androiddemopartone/act/HomeActivity.java
+++ AndroidDemoPartOne/app/src/main/java/com/example/idea/androiddemopartone/act/HomeActivity.java
@@ -1,10 +1,17 @@
-package com.example.idea.androiddemopartone;
+package com.example.idea.androiddemopartone.act;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import com.example.idea.androiddemopartone.R;
+
+import butterknife.Bind;
 
 public class HomeActivity extends AppCompatActivity {
 
+    @Bind(R.id.lv_list)
+    ListView lv_list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
