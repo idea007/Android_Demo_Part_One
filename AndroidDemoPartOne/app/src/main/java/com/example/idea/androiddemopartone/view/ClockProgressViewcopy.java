757803c14115d8e83f48dbbcf2e31a3bf3a9fe19
idea <1063292693@qq.com>
2016-04-29 16:45:29 +0800

    update
    update

--- AndroidDemoPartOne/app/src/main/java/com/example/idea/androiddemopartone/view/ClockProgressViewcopy.java
+++ AndroidDemoPartOne/app/src/main/java/com/example/idea/androiddemopartone/view/ClockProgressViewcopy.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -12,30 +13,28 @@ import com.example.idea.androiddemopartone.R;
 /**
  * Created by idea on 16/4/29.
  */
-public class ClockProgressView extends View {
+public class ClockProgressViewcopy extends View {
 
     private Paint paint;
+    private float mBarSize;
     private float mBarThickness;  //厚度
-    private int mSize;      //view 中间白色
+    private int mSize;
 
-    private int progress = 0;
-
-    private int height;  //view的高度
-    private int width;   //view的宽度
+    private int progress=0;
 
 
-    public ClockProgressView(Context context) {
+    public ClockProgressViewcopy(Context context) {
         super(context);
         init(context);
     }
 
 
-    public ClockProgressView(Context context, AttributeSet attrs) {
+    public ClockProgressViewcopy(Context context, AttributeSet attrs) {
         super(context, attrs);
         init(context);
     }
 
-    public ClockProgressView(Context context, AttributeSet attrs, int defStyleAttr) {
+    public ClockProgressViewcopy(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init(context);
     }
@@ -45,26 +44,6 @@ public class ClockProgressView extends View {
         super.onDraw(canvas);
         drawBg(canvas, paint); // 绘制表盘
         drawBar(canvas, paint); // 绘制时针、分针、秒针
-        drawPlusAndMinus(canvas, paint);
-
-    }
-
-    private void drawPlusAndMinus(Canvas canvas, Paint paint) {
-        canvas.save();
-        canvas.drawLine(width / 24, mSize / 2, width / 8, mSize / 2, paint);
-        canvas.drawLine(width * 23 / 24, mSize / 2, width * 7 / 8, mSize / 2, paint);
-        canvas.drawLine(width * 22 / 24, mSize / 2 - width / 24, width * 22 / 24, mSize / 2 + width / 24, paint);
-        canvas.restore();
-
-    }
-
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        this.height = getMeasuredHeight();
-        this.width = getMeasuredWidth();
-        mSize = width * 2 / 3;
-
 
     }
 
@@ -72,27 +51,45 @@ public class ClockProgressView extends View {
 
         paint.setColor(Color.BLACK);
         canvas.save();
-        canvas.rotate(progress, width / 2, mSize / 2);
-        canvas.drawLine(width / 2, mSize / 2, width / 4, mSize / 2, paint);
+        canvas.rotate(progress,mSize/2,mSize/2);
+//        Path path=new Path();
+//        path.moveTo(mSize/2,mSize/2);
+//        path.lineTo(mSize/2,mSize*3/8);
+        canvas.drawLine(mSize/2,mSize/2,mSize/8,mSize/2,paint);
+//
+//
+//        canvas.drawPath(path,paint);
+
 
         canvas.restore();
+
+        // 时针
+        paint.setColor(Color.BLUE);
+        canvas.save(); // 线锁定画布
+        canvas.rotate(30, mSize / 2, mSize / 2); // 旋转画布
+        Path path1 = new Path();
+        path1.moveTo(mSize / 2, mSize / 2); // 开始的基点
+        path1.lineTo(mSize / 2, mSize / 4); // 最后的基点
+        canvas.drawPath(path1, paint);
+        canvas.restore();
+
 
     }
 
     private void drawBg(Canvas canvas, Paint paint) {
 
         paint.setColor(Color.WHITE);
-        canvas.drawCircle(width / 2, mSize / 2, mSize * 7 / 16, paint);
+        canvas.drawCircle(mSize/2,mSize/2,mSize*7/16,paint);
 
     }
 
-    public void setProgress(int progress) {
-        if (progress < 0) {
-            progress = 0;
-        } else if (progress > 180) {
-            progress = 180;
+    public void setProgress(int progress){
+        if(progress<0){
+            progress=0;
+        }else if(progress>180){
+            progress=180;
         }
-        this.progress = progress;
+        this.progress=progress;
 
 
         invalidate();
@@ -100,12 +97,13 @@ public class ClockProgressView extends View {
     }
 
     private void init(Context context) {
-        paint = new Paint();
+        paint=new Paint();
 
 
         this.mSize = context.getResources().getDimensionPixelSize(R.dimen.ldrawer_drawableSize);
-
+        this.mBarSize = context.getResources().getDimensionPixelSize(R.dimen.ldrawer_barSize);
         this.mBarThickness = context.getResources().getDimensionPixelSize(R.dimen.ldrawer_thickness);
+
 
 
         paint.setAntiAlias(true);
